3d674a3c2fcd8ccbb09c5e33de74dda121a07dc1
Jaime Martínez Rincón <mrjaime1999@gmail.com>
2019-05-16 10:08:04 +0200

    Connect to losant through mqtts
    Backported b4f2ed9617126d999d582309547fd503620f6523

--- reflash.sh
+++ reflash.sh
@@ -1,9 +1,26 @@
 #!/bin/bash
 
+set -e
 source .env
 
-# --local --verbose --repo ../mongoose-os &&
-mos build &&
-  mos flash &&
-  mos wifi ${WIFI_SSID} "${WIFI_PSK}" &&
-  mos console
+# builds the firmware
+mos build
+
+# flashes the firmware
+mos flash
+
+# sets all our configuration options
+mos config-set \
+  dash.enable=true \
+  dash.token=${DASH_TOKEN} \
+  mqtt.client_id=${MQTT_CLIENT_ID} \
+  mqtt.user=${MQTT_USER} \
+  mqtt.pass=${MQTT_PASS}
+
+# configures the wifi
+mos wifi ${WIFI_SSID} "${WIFI_PSK}"
+
+# starts up the console
+mos console
+
+exit 0
